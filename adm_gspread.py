506d4cee44820039a204be8c02f09d8fa0b7cf02
Isaac Stoutenburgh <isaac.stoutenburgh@phoenix.k12.or.us>
2020-06-25 08:59:37 -0700

    created to_csv function

--- adm_gspread.py
+++ adm_gspread.py
@@ -26,7 +26,6 @@ def find_missing_ssid(list_of_dicts_in):
         Returns: list
             list of worksheet rows; one row of complete ADM data for each record missing an SSID
     """
-
     return list(filter(lambda missing: missing['ChkDigitStdntID'] == '', list_of_dicts_in))
 
 
@@ -40,7 +39,6 @@ def find_missing_data(list_of_dicts_in, column_name_to_check):
         Returns: list
             list of worksheet rows; one row of complete ADM data for each record missing data in the specified column
     """
-
     return list(filter(lambda missing: missing[column_name_to_check] == '', list_of_dicts_in))
 
 
@@ -51,7 +49,6 @@ def find_all_missing_data(list_of_dicts_in):
          Returns: list
              list of worksheet rows; one row of complete ADM data for each record missing data in the specified column
      """
-
     column_names = list(list_of_dicts_in[0].keys())
     missing_val_list = []
     for name in column_names:
@@ -60,14 +57,27 @@ def find_all_missing_data(list_of_dicts_in):
     ret_val = [i for n, i in enumerate(missing_val_list) if i not in missing_val_list[n + 1:]]
     return ret_val
 
-pprint.pp(find_all_missing_data(list_of_dicts))
 
-toCSV =find_all_missing_data(list_of_dicts)
-keys = toCSV[0].keys()
-with open('isaac_testing.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(toCSV)
+def to_csv(list_of_dicts_in, name_of_csv_to_create):
+    """ Function that takes a list of dictionaries and creates a csv file.
+    Parameter: list of dictionaries
+        list_of_dicts_in; the list of dictionaries to create a csv out of
+    Parameter: string
+        name_of_csv_to_create; this will be the name of the resulting csv file - NOTE: include .csv
+    Returns: no return value
+        will create a csv file in current directory
+    """
+    list_to_csv = find_all_missing_data(list_of_dicts_in)
+    keys = list_to_csv[0].keys()
+    with open(name_of_csv_to_create, 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(list_to_csv)
+
+
+missing_data = find_all_missing_data(list_of_dicts)
+to_csv(missing_data, "isaac_test.csv")
+
 
 
 
