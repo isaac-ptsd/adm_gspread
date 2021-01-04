368f8889276df7801b9a96cead2bf58f063b0ed9
Isaac Stoutenburgh <isaac.stoutenburgh@phoenix.k12.or.us>
2021-01-04 13:44:04 -0800

    cleaned up name, added sleep call

--- adm_verification_prog.py
+++ adm_verification_prog.py
@@ -1,4 +1,4 @@
-import adm_library
+import adm_library as adm
 import gspread
 from gspread import Spreadsheet
 from gspread import utils
@@ -14,45 +14,46 @@ import pprint
 
 
 def main():
-    list_of_dicts = adm_library.gen_list_of_dicts()
-    # adm_library.add_wsheet(adm_library.check_non_type2_dups(list_of_dicts), "duplicates_exclude_type2")
-    #
-    # print("\nChecking for SpEd Students:")
-    # adm_library.add_wsheet(adm_library.generate_sped_list(list_of_dicts), "SpEd_students")
-    # adm_library.add_wsheet(adm_library.find_no_dup_sped(list_of_dicts), "SpEd_no_dup_record")
-    #
-    # print("\nCalculating ADM Amount:")
-    # calculate_update_calcadmamt(list_of_dicts)
-    #
-    # print("\nChecking for missing data:")
-    # add_wsheet(find_all_missing_data(list_of_dicts), "records_missing_data")
-    #
-    # print("\nChecking ethnic flags:")
-    # add_wsheet(check_eth_flags(list_of_dicts), "missing_eht_flag")
-    #
-    # print("\nChecking KG - 8 for econ EconDsvntgFg set to 'Y':")
-    # add_wsheet(check_econ_flag_k8(list_of_dicts), "k8_N_econ_flag")
-    #
-    # print("\nChecking for attendance anomalies:")
-    # add_wsheet(find_attendance_anomalies(list_of_dicts), "attendance_anomalies")
-    #
-    # print("\nChecking for ADM program type 14 students:")
-    # check_admprog_type_14(list_of_dicts)
-    #
-    # print("\nChecking for ADM program type 2 students:")
-    # check_admprog_type_2(list_of_dicts)
-    #
-    # print("\nChecking for type 2 matches:")
-    # add_wsheet(check_elfg(list_of_dicts), "no_matching_ADMProgTypCd2")
+    list_of_dicts = adm.gen_list_of_dicts()
+    adm.add_wsheet(adm.check_non_type2_dups(list_of_dicts), "duplicates_exclude_type2")
 
-    # print("\nComparing student count to calculated ADM amount:")
-    # adm_library.compare_calcadm_school_counts(list_of_dicts)
+    print("\nChecking for SpEd Students:")
+    adm.add_wsheet(adm.generate_sped_list(list_of_dicts), "SpEd_students")
+    adm.add_wsheet(adm.find_no_dup_sped(list_of_dicts), "SpEd_no_dup_record")
+
+    print("\nCalculating ADM Amount:")
+    adm.calculate_update_calcadmamt(list_of_dicts)
+
+    print("\nChecking for missing data:")
+    adm.add_wsheet(adm.find_all_missing_data(list_of_dicts), "records_missing_data")
+
+    print("\nChecking ethnic flags:")
+    adm.add_wsheet(adm.check_eth_flags(list_of_dicts), "missing_eht_flag")
+
+    print("\nChecking KG - 8 for econ EconDsvntgFg set to 'Y':")
+    adm.add_wsheet(adm.check_econ_flag_k8(list_of_dicts), "k8_N_econ_flag")
+
+    print("\nChecking for attendance anomalies:")
+    adm.add_wsheet(adm.find_attendance_anomalies(list_of_dicts), "attendance_anomalies")
+
+    print("\nChecking for ADM program type 14 students:")
+    adm.check_admprog_type_14(list_of_dicts)
+
+    print("\nChecking for ADM program type 2 students:")
+    adm.check_admprog_type_2(list_of_dicts)
+
+    print("\nChecking for type 2 matches:")
+    adm.add_wsheet(adm.check_elfg(list_of_dicts), "no_matching_ADMProgTypCd2")
+
+    print("\nComparing student count to calculated ADM amount:")
+    adm.compare_calcadm_school_counts(list_of_dicts)
 
     print("records with no attendance")
-    adm_library.check_for_no_att(list_of_dicts)
+    adm.check_for_no_att(list_of_dicts)
 
     print("records with enrolled date after end date")
-    adm_library.enrolled_after_end(list_of_dicts)
+    time.sleep(2)
+    adm.enrolled_after_end(list_of_dicts)
 
 
 if __name__ == '__main__':
